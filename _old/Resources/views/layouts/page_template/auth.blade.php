553b5915c251afbb5dcef88c714875fc1d3dfc5a
marco76tv <marco.sottana@gmail.com>
2024-05-08 21:09:01 +0200

    up

--- _old/Resources/views/layouts/page_template/auth.blade.php
+++
@@ -1,9 +0,0 @@
-<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-    @csrf
-</form>
-@include('pub_theme::layouts.navbars.sidebar')
-<div class="main-panel">
-    @include('pub_theme::layouts.navbars.navs.auth')
-    @yield('content')
-    @include('pub_theme::layouts.footer')
-</div>
