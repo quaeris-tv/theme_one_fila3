2736e4e1ef48b575d9540bc583f249e3ab5bf2dc
marco76tv <marco.sottana@gmail.com>
2025-02-28 18:07:03 +0100

    up

--- layouts/base.blade.php
+++ layouts/base.blade.php
@@ -24,6 +24,7 @@
         @filamentStyles
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         @vite(['resources/sass/app.scss'],'themes/One/dist')
 =======
         @vite(['Resources/sass/app.scss'],'themes/One/dist')
@@ -31,6 +32,9 @@
 =======
         @vite(['Resources/sass/app.scss'],'themes/One/dist')
 >>>>>>> d703c8f (up)
+=======
+        @vite(['resources/sass/app.scss'],'themes/One/dist')
+>>>>>>> bc5f829 (up)
 
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -44,6 +48,7 @@
         @filamentScripts
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         @vite(['resources/js/app.js'],'themes/One/dist')
 =======
         @vite(['Resources/js/app.js'],'themes/One/dist')
@@ -51,5 +56,8 @@
 =======
         @vite(['Resources/js/app.js'],'themes/One/dist')
 >>>>>>> d703c8f (up)
+=======
+        @vite(['resources/js/app.js'],'themes/One/dist')
+>>>>>>> bc5f829 (up)
     </body>
 </html>
